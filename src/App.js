e3c84312d81d262e9f884048b2bf038bc824779c
chxmek <Chawanwits.mek@gmail.com>
2024-10-02 14:39:24 +0700

    add routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Register from "./components/pages/auth/Register";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1> Hello React</h1>
-      <Register />
+      <Routes>
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
   );
 }
