88896e64536964a09dbfb9b791b128821c868914
chxmek <Chawanwits.mek@gmail.com>
2024-10-02 16:47:53 +0700

    add navbar antdesign

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { UserAddOutlined, UserOutlined } from "@ant-design/icons";
+import { Menu } from "antd";
+import { Link } from "react-router-dom";
 
 function Navbar() {
+  const [current, setCurrent] = useState("mail");
+  const onClick = (e) => {
+    console.log("click ", e);
+    setCurrent(e.key);
+  };
+  const items = [
+    {
+      label: <Link to="/login">Login</Link>,
+      key: "mail",
+      icon: <UserOutlined />,
+    },
+    {
+      label: <Link to="/register">Register</Link>,
+      key: "app",
+      icon: <UserAddOutlined />,
+    },
+  ];
   return (
     <div>
       <h1>Navbar</h1>
+      <Menu mode="horizontal" items={items} />;
     </div>
   );
 }
