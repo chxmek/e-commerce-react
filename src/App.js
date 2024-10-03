673b9f20d4e1a85ac51384d24899487f99ad76b9
chxmek <Chawanwits.mek@gmail.com>
2024-10-03 15:46:09 +0700

    add fetch token

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import Register from "./components/pages/auth/Register";
 import Login from "./components/pages/auth/Login";
 import Home from "./components/pages/Home";
@@ -5,8 +6,19 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/layouts/Navbar";
 import HomeAdmin from "./components/pages/admin/Home";
 import HomeUser from "./components/pages/user/Home";
+import { currentUser } from "./components/functions/auth";
 
 function App() {
+  const idtoken = localStorage.token;
+  if (idtoken) {
+    currentUser(idtoken)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
   return (
     <div className="App">
       <Navbar />
--- src/components/functions/auth.js
+++ src/components/functions/auth.js
@@ -7,3 +7,15 @@ export async function register(value) {
 export async function login(value) {
   return await axios.post(process.env.REACT_APP_API + "/login", value);
 }
+
+export async function currentUser(authtoken) {
+  return await axios.post(
+    process.env.REACT_APP_API + "/current-user",
+    {},
+    {
+      headers: {
+        authtoken,
+      },
+    }
+  );
+}
